5fd560ed1ef1aa17b76ed7a20b08c71ad6e3a0aa
M2k555 <24674@srithatpit.ac.th>
2025-01-24 15:45:25 +1100

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HYMLเบื้องต้น</title>
+</head>
+<body>
+  <p>------การเเสดงข้อความเเบบหัวข้อ------</p>
+  </div>
+   <h1>HTMLเบื้องต้น(h1)</h1>
+ </div>
+ <div>
+ <h2>HTMLเบื้องต้น(h1)</h2>
+ <h3>HTMLเบื้องต้น(h1)</h3>
+ <h4>HTMLเบื้องต้น(h1)</h4>
+ <h5>HTMLเบื้องต้น(h1)</h5>
+ <h6>HTMLเบื้องต้น(h1)</h6>
+ </div>
+ <p>------การเเสดงตัวหนา ตัวเอี่ยง การขีดเส้นใต้ข้อความ------</p>
+
+ <div>
+     <b>ปุญญพัฒน์ เชิญชม</b><br>
+     <i>ปุญญพัฒน์ เชิญชม</i><br>
+     <u>ปุญญพัฒน์ เชิญชม</u><br>
+ </div>
+ <p></b>-----การ link -----</b><p>
+ <div>
+     <!--link หน้าเดียว-->
+     <a href = "https://www.google.co.th">Google<</a><br>
+    <div>
+        
+</body>
+</html>
+
